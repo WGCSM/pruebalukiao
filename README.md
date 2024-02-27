863aced3cb0a3cec6c0cc66464e79d4894dd0668
WGCSM <whoknocksonthedoor@gmail.com>
2024-02-27 01:18:45 -0500

    readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # pruebalukiao
+
+1. Crear proyecto de laravel en servidor local.
+2. Correr migraciones: php artisan migrate.
+3. Correr comandos: php artisan serve y npm run dev.
+3. Registrarse como usuario en ingresar.
+4. Hacer click en boton de tabla de usuarios.
--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import { Head,Link }from '@inertiajs/react';
 
 export default function Dashboard({ auth }) {
     return (
@@ -12,7 +12,9 @@ export default function Dashboard({ auth }) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
+                        <div className="flex items-center justify-between mb-6">
+                            <Link className="px-6 py-2 text-white bg-blue-500 rounded-md focus:outline-none" href={ route("empleados.index") }>Tabla de Empleados</Link>
+                        </div>
                     </div>
                 </div>
             </div>
